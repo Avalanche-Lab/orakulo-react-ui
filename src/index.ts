b1a78b40836174b5e32e9414bbc221804d4c5e7b
Lucas-A-Araujo <lucas.al.araujo10@gmail.com>
2025-07-09 17:17:51 -0300

    feat: export styles to user

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Exporta todos os componentes
+export * from "./components";
+
+// Exporta os estilos (para uso opcional)
+export { default as styles } from "./styles/index.css";
+export { default as theme } from "./styles/theme.css";
