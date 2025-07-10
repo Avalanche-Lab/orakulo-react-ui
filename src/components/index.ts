bcb5ef1e428436b02414119b3e4098634f173117
Lucas-A-Araujo <lucas.al.araujo10@gmail.com>
2025-07-10 18:55:02 -0300

    chore: remove unnecessary files

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export { Badge } from "./Badge";
 export { Button } from "./Button";
 export { Checkbox } from "./Checkbox";
 export { Input } from "./Input";
+export { Modal } from "./Modal";
 export { Select } from "./Select";
 export { Textarea } from "./Textarea";
