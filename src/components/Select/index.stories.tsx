8e4fa1520950d8d32497a197ec86e7e7ff1cfefa
Lucas-A-Araujo <lucas.al.araujo10@gmail.com>
2025-07-10 17:01:04 -0300

    feat: create select button story

---
+++ src/components/Select/index.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Select } from ".";
+
+const meta: Meta<typeof Select> = {
+  title: "Components/Select",
+  component: Select,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Text: Story = {
+  args: {
+    label: "Select",
+    options: [
+      { value: "1", label: "Option 1" },
+      { value: "2", label: "Option 2" },
+      { value: "3", label: "Option 3" },
+    ],
+  },
+};
+
+export const Password: Story = {
+  args: {
+    label: "Select",
+    options: [
+      { value: "1", label: "Option 1" },
+      { value: "2", label: "Option 2" },
+      { value: "3", label: "Option 3" },
+    ],
+  },
+};
+
+export const Number: Story = {
+  args: {
+    label: "Select",
+    options: [
+      { value: "1", label: "Option 1" },
+      { value: "2", label: "Option 2" },
+      { value: "3", label: "Option 3" },
+    ],
+  },
+};
+
+export const Date: Story = {
+  args: {
+    label: "Select",
+    options: [
+      { value: "1", label: "Option 1" },
+      { value: "2", label: "Option 2" },
+      { value: "3", label: "Option 3" },
+    ],
+  },
+};
